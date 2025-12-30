8d746f84a07bfdd5d435df706dc4e2d9835bff09
himanshukumar <yaduvanshier05@gmail.com>
2025-12-30 22:41:18 +0530

    Commit J-App

--- src/main/java/net/engineeringdigest/journalApp/service/UserDetailServiceImpl.java
+++ src/main/java/net/engineeringdigest/journalApp/service/UserDetailServiceImpl.java
@@ -3,17 +3,25 @@ package net.engineeringdigest.journalApp.service;
 import net.engineeringdigest.journalApp.Repository.UserRepo;
 import net.engineeringdigest.journalApp.entity.Users;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class UserDetailServiceImpl implements UserDetailsService {
 
     @Autowired
     UserRepo userRepo;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -28,5 +36,13 @@ public class UserDetailServiceImpl implements UserDetailsService {
 
         }
    throw  new UsernameNotFoundException("Username not found: " + username);
+    }
+
+
+    public List<Users> getUserForSA() {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}$"));
+        query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
+        return mongoTemplate.find(query, Users.class);
     }
 }
